59ddce8c0c07146bcfe1fd8b8985ab2a9f7c04e1
Kaushikidum29 <kaushikidum29@gmail.com>
2024-09-21 00:05:15 +0530

    space

--- lib/userlogin_page.dart
+++ lib/userlogin_page.dart
@@ -74,6 +74,7 @@ class _UserloginPageState extends State<UserloginPage> {
                               borderRadius: BorderRadius.circular(20),
                               borderSide: BorderSide(
                                   color: Colors.grey.shade300, width: 1)),
+
                           prefixIcon: Icon(
                             Icons.person,
                             color: Colors.grey,

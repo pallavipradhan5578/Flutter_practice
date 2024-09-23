8f57a83bce1d8d004fecea5623b5d99c7b90a2ce
pallavipradhan5578 <pallavipradhan5578@gmail.com>
2024-09-23 18:33:33 +0530

    Listview widget.dart

--- lib/main.dart
+++ lib/main.dart
@@ -8,7 +8,7 @@ void main() {
   runApp(MaterialApp(debugShowCheckedModeBanner: false,theme: ThemeData(
     primarySwatch: Colors.red
   ),
-      home:  ListviewBuilder()
+      home:  ListViewWidget()
 
 
       //   Scaffold(

1e34208c36e62da9ea99e3e685c1e7166bab98ca
pallavipradhan5578 <pallavipradhan5578@gmail.com>
2024-09-26 20:47:23 +0530

    card widget practics.dart

--- lib/card_&_textfild_widget.dart
+++ lib/card_&_textfild_widget.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+
 class practicsCardTextfield extends StatefulWidget {
   const practicsCardTextfield({super.key});
 
@@ -9,29 +10,75 @@ class practicsCardTextfield extends StatefulWidget {
 class _practicsCardTextfieldState extends State<practicsCardTextfield> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(appBar: AppBar(backgroundColor: Colors.blue,
-      actions: [
-        IconButton(onPressed: (){}, icon: Icon(Icons.notifications)),
-        IconButton(onPressed: (){}, icon: Icon(Icons.filter_alt_outlined))
-      ],
-    ),drawer: Drawer(),
-      body: Padding(
-        padding: const EdgeInsets.all(18.0),
-        child: TextField(decoration: InputDecoration(
-          hintText: "Search your product",focusedBorder: OutlineInputBorder(
-          borderRadius: BorderRadius.circular(15),borderSide:
-    BorderSide(color: Colors.blue,width: 2),
-        ),enabledBorder: OutlineInputBorder(
-          borderRadius: BorderRadius.circular(15),
-          borderSide: BorderSide(
-            color: Colors.grey
-      )
-
-
-        ),
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.blue,
+        actions: [
+          IconButton(onPressed: () {}, icon: Icon(Icons.notifications)),
+          IconButton(onPressed: () {}, icon: Icon(Icons.filter_alt_outlined))
+        ],
       ),
+      drawer: Drawer(),
+      body: Padding(
+          padding: const EdgeInsets.all(18.0),
+          child: Column(
+            children: [
+              TextField(
+                decoration: InputDecoration(
+                  hintText: "Search your product",
+                  focusedBorder: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(15),
+                    borderSide: BorderSide(color: Colors.blue, width: 2),
+                  ),
+                  enabledBorder: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(15),
+                      borderSide: BorderSide(color: Colors.grey)),
+                ),
+              ),
 
-    ))
-    ,backgroundColor:Colors.white,);
+              Padding(
+                padding: const EdgeInsets.all(18.0),
+                child: Container(height: 200,width: 200,
+                  child: Card(shadowColor: Colors.blue,
+                    elevation: 12,child: Center(child: Padding(
+                      padding: const EdgeInsets.only(left: 40.0),
+                      child: Text("Hello World",style: TextStyle(fontSize: 29,
+                          fontWeight: FontWeight.bold),),
+                    )),color: Colors.cyan,
+
+                  ),
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.all(18.0),
+                child: Container(height: 200,width: 200,
+                  child: Card(shadowColor: Colors.blue,
+                    elevation: 12,child: Center(child: Padding(
+                      padding: const EdgeInsets.only(left: 40.0),
+                      child: Text("Hello World",style: TextStyle(fontSize: 29,
+                          fontWeight: FontWeight.bold),),
+                    )),color: Colors.cyan,
+
+                  ),
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.all(18.0),
+                child: Container(height: 200,width: 200,
+                  child: Card(shadowColor: Colors.blue,
+                    elevation: 12,child: Center(child: Padding(
+                      padding: const EdgeInsets.only(left: 40.0),
+                      child: Text("Hello World",style: TextStyle(fontSize: 29,
+                          fontWeight: FontWeight.bold),),
+                    )),color: Colors.cyan,
+
+                  ),
+                ),
+              ),
+
+            ],
+          )),
+      backgroundColor: Colors.white,
+    );
   }
 }

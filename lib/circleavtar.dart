482b838d003b2c9b626811cabb9795d3f2d3df44
pallavipradhan5578 <pallavipradhan5578@gmail.com>
2024-09-27 18:55:57 +0530

    How to get current date & time prg.dart

--- lib/circleavtar.dart
+++ lib/circleavtar.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+
 class Cicleavtarpractics extends StatefulWidget {
   const Cicleavtarpractics({super.key});
 
@@ -9,16 +10,24 @@ class Cicleavtarpractics extends StatefulWidget {
 class _CicleavtarpracticsState extends State<Cicleavtarpractics> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(body: Center(
-      child: CircleAvatar(backgroundColor: Colors.blue.shade300,maxRadius: 200,
-        child: CircleAvatar(backgroundColor: Colors.blue,child: Text("Hello World",style: TextStyle
-          (fontWeight: FontWeight.bold,color: Colors.white,fontSize: 21,fontStyle: FontStyle.italic),),radius: 100,
-
-
-        ),
-      ),
+    var time = DateTime.now();
+    return Scaffold(appBar: AppBar(
+      title: Text("How to get current date & time "),backgroundColor: Colors.blue,
     ),
+      body: Container(height: 200,width: 200,
+        child: Column(mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+          Text(
+            "Current Time : $time.Year",
+            style: TextStyle(fontSize: 21),
+          ),
+              ElevatedButton(onPressed: (){
+            setState(() {
 
+            });
+              },child: Text("Current Time"),  )
+        ]),
+      ),
     );
   }
 }

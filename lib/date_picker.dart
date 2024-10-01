38d7d1f558472f8eed08ab43e2c4f279e0402c99
pallavipradhan5578 <pallavipradhan5578@gmail.com>
2024-10-01 17:09:51 +0530

    GrideView_extent.dart

--- lib/date_picker.dart
+++ lib/date_picker.dart
@@ -11,68 +11,68 @@ class _DatePickerState extends State<DatePicker> {
   @override
   Widget build(BuildContext context) {
     var arrColors = [
-    Colors.red,
-    Colors.green,
-    Colors.grey,
-    Colors.amber,
-    Colors.purple,
-    Colors.blue,
-    Colors.red.shade300,
-    Colors.brown,
-    Colors.deepOrange,
-    Colors.pink,
+      Colors.red,
+      Colors.green,
+      Colors.grey,
+      Colors.amber,
+      Colors.purple,
+      Colors.blue,
+      Colors.red.shade300,
+      Colors.brown,
+      Colors.deepOrange,
+      Colors.pink,
+    ];
 
-  ];
+    return Scaffold(
+        appBar: AppBar(
+          backgroundColor: Colors.grey.shade300,
+          title: Text("GridView.Count"),
+        ),
+        body:
+          Column(
+            children: [
+              Container(
+                  height: 300,
+                  child: GridView.extent(
+                    maxCrossAxisExtent: 100,
+                    crossAxisSpacing: 11,
+                    mainAxisSpacing: 11,
+                    children: [
+                      Container(
+                        color: arrColors[0],
+                      ),
+                      Container(
+                        color: arrColors[1],
+                      ),
+                      Container(
+                        color: arrColors[2],
+                      ),
+                      Container(
+                        color: arrColors[3],
+                      ),
+                      Container(
+                        color: arrColors[4],
+                      ),
+                      Container(
+                        color: arrColors[5],
+                      ),
+                      Container(
+                        color: arrColors[6],
+                      ),
+                      Container(
+                        color: arrColors[7],
+                      ),
+                      Container(
+                        color: arrColors[8],
+                      ),
+                      Container(
+                        color: arrColors[9],
+                      ),
+                    ],
+                  )),
 
-    return Scaffold(appBar: AppBar(backgroundColor: Colors.grey.shade300,
-    title:Text ("GridView.Count"),),
-      body: Container(width: 300,
-        child: GridView.count(crossAxisCount: 3,
-        children: [
-
-          Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: Container(color: arrColors[0],),
-          ),
-          Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: Container(color: arrColors[1],),
-          ),
-          Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: Container(color: arrColors[2],),
-          ),
-          Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: Container(color: arrColors[3],),
-          ),
-          Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: Container(color: arrColors[4],),
-          ),
-          Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: Container(color: arrColors[5],),
-          ),
-          Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: Container(color: arrColors[6],),
-          ),
-          Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: Container(color: arrColors[7],),
-          ),
-          Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: Container(color: arrColors[8],),
-          ),
-          Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: Container(color: arrColors[9],),
+            ],
           ),
 
-        ],),
-      ),
-    );
-  }
+    );}
 }

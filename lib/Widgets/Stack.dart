c6c8b26164c0e363261abec636f87d278bbc9cc8
pallavipradhan5578 <pallavipradhan5578@gmail.com>
2024-10-04 12:58:12 +0530

    Card .dart

--- lib/Widgets/Stack.dart
+++ lib/Widgets/Stack.dart
@@ -10,21 +10,57 @@ class _StackWidgetState extends State<StackWidget> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Stack(
+      body: SingleChildScrollView(scrollDirection: Axis.vertical,
+        child: Column(
           children: [
-            Container(
-              width:300 ,height: 300,color: Colors.green,
+            Center(
+              child: Stack(
+                children: [
+                  Container(
+                    width:300 ,height: 300,color: Colors.blueGrey,
 
-            ),
-            Positioned(left: 50,right: 50,top: 50,bottom: 50,
-              child: Container(
-                   // width:50 ,height: 50,
-                color: Colors.black,
-                child: IconButton(onPressed: (){}, icon: Icon(Icons.add,color: Colors.white,)),
+                  ),
+                  Positioned(left: 50,right: 50,top: 50,bottom: 50,
+                    child: Container(
+                         // width:50 ,height: 50,
+                      color: Colors.grey,
+                      child: IconButton(onPressed: (){}, icon: Icon(Icons.add,color: Colors.white,)),
 
-                    ),
-            ),
+                          ),
+                  ),
+                ],
+              ),
+            ),Card(
+            child: Container(height:300,width:300,color:Colors.white,child: Center(child: Text("Hello"))),color: Colors.purple,elevation: 12,
+            ),Card(
+            child: Container(height:300,width:300,color:Colors.red,child: Center(child: Text("Hello"))),color: Colors.purple,elevation: 12,
+            ),Card(
+            child: Container(height:300,width:300,color:Colors.green,child: Center(child: Text("Hello"))),color: Colors.purple,elevation: 12,
+            ),Card(
+            child: Container(height:100,width:100,color:Colors.blue,child: Center(child: Text("Hello"))),color: Colors.purple,elevation: 12,
+            ),Card(
+            child: Container(height:100,width:100,color:Colors.brown,child: Center(child: Text("Hello"))),color: Colors.purple,elevation: 12,
+            ),Card(
+            child: Container(height:100,width:100,color:Colors.white,child: Center(child: Text("Hello"))),color: Colors.purple,elevation: 12,
+            ),Card(
+            child: Container(height:100,width:100,color:Colors.red,child: Center(child: Text("Hello"))),color: Colors.purple,elevation: 12,
+            ),Card(
+            child: Container(height:100,width:100,color:Colors.green,child: Center(child: Text("Hello"))),color: Colors.purple,elevation: 12,
+            ),Card(
+            child: Container(height:100,width:100,color:Colors.blue,child: Center(child: Text("Hello"))),color: Colors.purple,elevation: 12,
+            ),Card(
+            child: Container(height:100,width:100,color:Colors.brown,child: Center(child: Text("Hello"))),color: Colors.purple,elevation: 12,
+            ),Card(
+            child: Container(height:100,width:100,color:Colors.white,child: Center(child: Text("Hello"))),color: Colors.purple,elevation: 12,
+            ),Card(
+            child: Container(height:100,width:100,color:Colors.red,child: Center(child: Text("Hello"))),color: Colors.purple,elevation: 12,
+            ),Card(
+            child: Container(height:100,width:100,color:Colors.green,child: Center(child: Text("Hello"))),color: Colors.purple,elevation: 12,
+            ),Card(
+            child: Container(height:100,width:100,color:Colors.blue,child: Center(child: Text("Hello"))),color: Colors.purple,elevation: 12,
+            ),Card(
+            child: Container(height:100,width:100,color:Colors.brown,child: Center(child: Text("Hello"))),color: Colors.purple,elevation: 12,
+            )
           ],
         ),
       ),

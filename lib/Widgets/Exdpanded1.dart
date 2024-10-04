61c08bd5fdeccab69e08d1464c1e7a4ad7910a53
pallavipradhan5578 <pallavipradhan5578@gmail.com>
2024-10-05 02:32:36 +0530

    Icon Widget.dart

--- lib/Widgets/Exdpanded1.dart
+++ lib/Widgets/Exdpanded1.dart
@@ -12,100 +12,24 @@ class _Exdpanded1State extends State<Exdpanded1> {
     return Scaffold(appBar: AppBar(
       title: Text('Scrooll view widget'),backgroundColor: Colors.blueAccent,
     ),
-      body: Padding(
-        padding: const EdgeInsets.all(8.0),
-
-        child: SingleChildScrollView(
-          child: Column(
-
-            children: [SingleChildScrollView(
-              scrollDirection: Axis.horizontal,
-              child: Row(
-                children: [
-
-                  Padding(
-                    padding: const EdgeInsets.all(8.0),
-                    child: Container(
-                      width: 100,
-                      height:200,
-                      color: Colors.green,
-                    ),),Padding(
-                    padding: const EdgeInsets.all(8.0),
-                    child: Container(
-                      width: 100,
-                      height:200,
-                      color: Colors.redAccent,
-                    ),),Padding(
-                    padding: const EdgeInsets.all(8.0),
-                    child: Container(
-                      width: 100,
-                      height:200,
-                      color: Colors.brown,
-                    ),),Padding(
-                    padding: const EdgeInsets.all(8.0),
-                    child: Container(
-                      width: 100,
-                      height:200,
-                      color: Colors.green,
-                    ),),Padding(
-                    padding: const EdgeInsets.all(8.0),
-                    child: Container(
-                      width: 100,
-                      height:200,
-                      color: Colors.grey,
-                    ),),
-                ],
+      body: Center(
+        child: Container(
+          width: 100,
+          height: 100,
+          color: Colors.yellow,
+          child: Align(
+            alignment: Alignment.bottomRight,
+            child: Center(
+              child: Icon(
+                Icons.star,
+                color: Colors.red,
+                size: 50,
               ),
             ),
-              Padding(
-
-                padding: const EdgeInsets.all(8.0),
-                child: Container(
-                  width: 300,
-                  height:200,
-                  color: Colors.cyan,
-                ),
-              ),
-              Padding(
-                padding: const EdgeInsets.all(8.0),
-                child: Container(
-                  width: 300,
-                  height:200,
-                  color: Colors.green,
-                ),
-              ),Padding(
-                padding: const EdgeInsets.all(8.0),
-                child: Container(
-                  width: 400,
-                  height:200,
-                  color: Colors.yellow,
-                ),
-              ),Padding(
-                padding: const EdgeInsets.all(8.0),
-                child: Container(
-                  width: 400,
-                  height:200,
-                  color: Colors.red,
-                ),
-              ),Padding(
-                padding: const EdgeInsets.all(8.0),
-                child: Container(
-                  width: 400,
-                  height:200,
-                  color: Colors.brown ,
-                ),
-              ),Padding(
-                padding: const EdgeInsets.all(8.0),
-                child: Container(
-                  width: 400,
-                  height:200,
-                  color: Colors.orange,
-                ),
-              )
-            ],
           ),
         ),
-      ),
+      )
+
     );
   }
 }

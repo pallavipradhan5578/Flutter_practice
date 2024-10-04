2681f1efbaa65796c5c72e0023984de56b279def
pallavipradhan5578 <pallavipradhan5578@gmail.com>
2024-10-05 02:25:44 +0530

    Practics Expanded Widget.dart

--- lib/Widgets/Exdpanded1.dart
+++ lib/Widgets/Exdpanded1.dart
@@ -9,21 +9,103 @@ class Exdpanded1 extends StatefulWidget {
 class _Exdpanded1State extends State<Exdpanded1> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(body: Column(
-      children: [
-        Expanded(flex: 2,
-            child: Container(width: 60,height:60,color:Colors.red,child: Text("hello"),)),
-        Expanded(flex:4,
-            child: Container(width: 60,height:60,color:Colors.brown,child: Text("hello"),)),
-        Expanded(flex: 3,
-            child: Container(width: 60,height:60,color:Colors.cyan,child: Text("hello"),)),
-
-        Expanded(
-            child: Container(width: 60,height:60,color:Colors.green,child: Text("hello"),)),
-
-      ],
+    return Scaffold(appBar: AppBar(
+      title: Text('Scrooll view widget'),backgroundColor: Colors.blueAccent,
     ),
+      body: Padding(
+        padding: const EdgeInsets.all(8.0),
 
+        child: SingleChildScrollView(
+          child: Column(
+
+            children: [SingleChildScrollView(
+              scrollDirection: Axis.horizontal,
+              child: Row(
+                children: [
+
+                  Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: Container(
+                      width: 100,
+                      height:200,
+                      color: Colors.green,
+                    ),),Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: Container(
+                      width: 100,
+                      height:200,
+                      color: Colors.redAccent,
+                    ),),Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: Container(
+                      width: 100,
+                      height:200,
+                      color: Colors.brown,
+                    ),),Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: Container(
+                      width: 100,
+                      height:200,
+                      color: Colors.green,
+                    ),),Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: Container(
+                      width: 100,
+                      height:200,
+                      color: Colors.grey,
+                    ),),
+                ],
+              ),
+            ),
+              Padding(
+
+                padding: const EdgeInsets.all(8.0),
+                child: Container(
+                  width: 300,
+                  height:200,
+                  color: Colors.cyan,
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Container(
+                  width: 300,
+                  height:200,
+                  color: Colors.green,
+                ),
+              ),Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Container(
+                  width: 400,
+                  height:200,
+                  color: Colors.yellow,
+                ),
+              ),Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Container(
+                  width: 400,
+                  height:200,
+                  color: Colors.red,
+                ),
+              ),Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Container(
+                  width: 400,
+                  height:200,
+                  color: Colors.brown ,
+                ),
+              ),Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Container(
+                  width: 400,
+                  height:200,
+                  color: Colors.orange,
+                ),
+              )
+            ],
+          ),
+        ),
+      ),
     );
   }
 }

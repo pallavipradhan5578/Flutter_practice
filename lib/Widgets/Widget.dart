983856aa2c2b0061f3348aa831fe42299f4a9dac
pallavipradhan5578 <pallavipradhan5578@gmail.com>
2024-10-06 20:34:58 +0530

    Icon widget.dart

--- lib/Widgets/Widget.dart
+++ lib/Widgets/Widget.dart
@@ -9,11 +9,15 @@ class WidgetPractics extends StatefulWidget {
 class _WidgetPracticsState extends State<WidgetPractics> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(body: Center(
-      child: Icon(
-        Icons.favorite,
-        size: 40.0,
-        color: Colors.red,
+    return Scaffold(body: Padding(
+      padding: const EdgeInsets.all(78.0),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: <Widget>[
+          Icon(Icons.home, size: 30),
+          Icon(Icons.search, size: 30),
+          Icon(Icons.settings, size: 30),
+        ],
       ),
     )
       ,

44877b9b8e108acb54579e4e52564dc4c8ac395b
pallavipradhan5578 <pallavipradhan5578@gmail.com>
2024-10-06 20:40:01 +0530

    send button.dart

--- lib/Widgets/Widget.dart
+++ lib/Widgets/Widget.dart
@@ -11,12 +11,23 @@ class _WidgetPracticsState extends State<WidgetPractics> {
   Widget build(BuildContext context) {
     return Scaffold(body: Padding(
       padding: const EdgeInsets.all(78.0),
-      child: Row(
-        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-        children: <Widget>[
-          Icon(Icons.home, size: 30),
-          Icon(Icons.search, size: 30),
-          Icon(Icons.settings, size: 30),
+      child: Column(
+        children: [
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: <Widget>[
+              Icon(Icons.home, size: 30),
+              Icon(Icons.search, size: 30),
+              Icon(Icons.settings, size: 30),
+            ],
+          ),Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: ElevatedButton.icon(
+                  onPressed: () {},
+                  icon: Icon(Icons.send),
+                  label: Text("Send"),
+                ),
+          )
         ],
       ),
     )

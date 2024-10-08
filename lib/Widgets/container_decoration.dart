75e254be6608081133b12b785e9edbaa3ec8ab19
pallavipradhan5578 <pallavipradhan5578@gmail.com>
2024-10-08 19:06:58 +0530

    CircleAvatar.dart

--- lib/Widgets/container_decoration.dart
+++ lib/Widgets/container_decoration.dart
@@ -12,13 +12,25 @@ class _ContainerDecorationState extends State<ContainerDecoration> {
   Widget build(BuildContext context) {
     return Scaffold(appBar: AppBar(
       backgroundColor: Colors.blueGrey,
-    ), body: Column(mainAxisAlignment: MainAxisAlignment.center,
-      children: [
-        Text("       Welcome Back",
-          style: TextStyle(fontSize: 40, fontWeight: FontWeight.bold,color: Colors.blue),),
-        Text("                      Enter your credential to login",
-          style: TextStyle(fontSize: 15,color: Colors.blue),),
-      ],
+    ), body: Padding(
+      padding: const EdgeInsets.all(18.0),
+      child: Column(mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Text("       Welcome Back",
+            style: TextStyle(fontSize: 40, fontWeight: FontWeight.bold,color: Colors.blue),),
+          Text("                      Enter your credential to login",
+            style: TextStyle(fontSize: 15,color: Colors.blue),),Center(
+            child: CircleAvatar(
+              //backgroundColor: Colors.blue,radius: 150,
+              backgroundImage: NetworkImage('https://cdn.pixabay.com/photo/2023/07/24/07/2'
+                  '4/flower-wallpaper-8146421_1280.png'),
+      //radius: 100,
+              maxRadius: 200,
+              minRadius: 100,
+            ),
+          ),
+        ],
+      ),
     )
       ,);
   }}

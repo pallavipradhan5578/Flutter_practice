4cd6cbfde9730d24ab3246ffc7ef968ffef713bd
pallavipradhan5578 <pallavipradhan5578@gmail.com>
2024-10-08 19:04:07 +0530

    Text_widget.dart

--- lib/Widgets/container_decoration.dart
+++ lib/Widgets/container_decoration.dart
@@ -10,48 +10,15 @@ class ContainerDecoration extends StatefulWidget {
 class _ContainerDecorationState extends State<ContainerDecoration> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('Flutter Card Example'),
-      ),
-      body: Center(
-        child: Card(
-          elevation: 5, // Elevation to give the card a shadow
-          shape: RoundedRectangleBorder(
-            borderRadius: BorderRadius.circular(15),
-          ),
-          color: Colors.lightBlueAccent, // Background color of the card
-          child: Container(
-            width: 300,
-            height: 150,
-            padding: EdgeInsets.all(20),
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: <Widget>[
-                Text(
-                  'Flutter Card Widget',
-                  style: TextStyle(
-                    fontSize: 20,
-                    fontWeight: FontWeight.bold,
-                    color: Colors.white,
-                  ),
-                ),
-                SizedBox(height: 10),
-                Text(
-                  'This is a simple card in Flutter.',
-                  style: TextStyle(
-                    fontSize: 16,
-                    color: Colors.white70,
-                  ),
-                  textAlign: TextAlign.center,
-                ),
-              ],
-            ),
-          ),
-        ),
-      ),
-
-
-    );
-  }
-}
+    return Scaffold(appBar: AppBar(
+      backgroundColor: Colors.blueGrey,
+    ), body: Column(mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        Text("       Welcome Back",
+          style: TextStyle(fontSize: 40, fontWeight: FontWeight.bold,color: Colors.blue),),
+        Text("                      Enter your credential to login",
+          style: TextStyle(fontSize: 15,color: Colors.blue),),
+      ],
+    )
+      ,);
+  }}

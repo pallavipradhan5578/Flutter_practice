1b03a0623811cca2c70a21ca48a2ec71ca8ade3c
pallavipradhan5578 <pallavipradhan5578@gmail.com>
2024-10-08 19:11:30 +0530

    widget practics.dart

--- lib/Widgets/container_decoration.dart
+++ lib/Widgets/container_decoration.dart
@@ -12,25 +12,22 @@ class _ContainerDecorationState extends State<ContainerDecoration> {
   Widget build(BuildContext context) {
     return Scaffold(appBar: AppBar(
       backgroundColor: Colors.blueGrey,
-    ), body: Padding(
-      padding: const EdgeInsets.all(18.0),
-      child: Column(mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          Text("       Welcome Back",
-            style: TextStyle(fontSize: 40, fontWeight: FontWeight.bold,color: Colors.blue),),
-          Text("                      Enter your credential to login",
-            style: TextStyle(fontSize: 15,color: Colors.blue),),Center(
-            child: CircleAvatar(
-              //backgroundColor: Colors.blue,radius: 150,
-              backgroundImage: NetworkImage('https://cdn.pixabay.com/photo/2023/07/24/07/2'
-                  '4/flower-wallpaper-8146421_1280.png'),
-      //radius: 100,
-              maxRadius: 200,
-              minRadius: 100,
-            ),
-          ),
-        ],
-      ),
+    ), body: Column(mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        Text("       Welcome Back",
+          style: TextStyle(fontSize: 40, fontWeight: FontWeight.bold,color: Colors.blue),),
+        Text("                      Enter your credential to login",
+          style: TextStyle(fontSize: 15,color: Colors.blue),),Expanded(flex: 2,
+            child: Container(width: 60,height:60,color:Colors.red,child: Text("hello"),)),
+        Expanded(flex:4,
+            child: Container(width: 60,height:60,color:Colors.brown,child: Text("hello"),)),
+        Expanded(flex: 3,
+            child: Container(width: 60,height:60,color:Colors.cyan,child: Text("hello"),)),
+
+        Expanded(
+            child: Container(width: 60,height:60,color:Colors.green,child: Text("hello"),)),
+
+      ],
     )
       ,);
   }}

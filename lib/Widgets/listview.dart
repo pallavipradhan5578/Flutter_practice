c45d1438083e97802bb295711cac7fad2f8d548a
pallavipradhan5578 <pallavipradhan5578@gmail.com>
2024-10-12 18:16:12 +0530

    ListView.builder.dart

--- lib/Widgets/listview.dart
+++ lib/Widgets/listview.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+
 class Listview extends StatefulWidget {
   const Listview({super.key});
 
@@ -6,168 +7,22 @@ class Listview extends StatefulWidget {
   State<Listview> createState() => _ListviewState();
 }
 
-class _ListviewState extends State<Listview> { var arrNames=['Siya','Diya','Miya','Riya','sona'];
+class _ListviewState extends State<Listview> {
+  var arrNames = ['Siya', 'Diya', 'Miya', 'Riya', 'sona'];
   @override
   Widget build(BuildContext context) {
-    return Scaffold(appBar: AppBar(
-      title: Text("Hello",style: TextStyle(fontWeight:FontWeight.bold,color: Colors.white ),),backgroundColor: Colors.brown,
-    ),body: SingleChildScrollView(scrollDirection: Axis.vertical,
-      child: Column(
-        children: [
-          Container(
-            height: 100,
-            width: 200,
-            decoration: BoxDecoration(
-                color: Colors.pinkAccent,
-                //borderRadius: BorderRadius.only(
-                //  topLeft: Radius.circular(21), bottomRight: Radius.circular(25)
-                // ),
-                border: Border.all(
-                    width: 2,
-                    color: Colors.cyan
-                ),
-                boxShadow: [
-                  BoxShadow(
-                    blurRadius: 11,
-                    spreadRadius: 7,
-                  )
-                ],shape: BoxShape.rectangle ),
-          ),
-          Container(
-            height: 100,
-            width: 200,
-            decoration: BoxDecoration(
-                color: Colors.greenAccent,
-                //borderRadius: BorderRadius.only(
-                //  topLeft: Radius.circular(21), bottomRight: Radius.circular(25)
-                // ),
-                border: Border.all(
-                    width: 2,
-                    color: Colors.cyan
-                ),
-                boxShadow: [
-                  BoxShadow(
-                    blurRadius: 11,
-                    spreadRadius: 7,
-                  )
-                ],shape: BoxShape.rectangle ),
-          ), Container(
-            height: 100,
-            width: 200,
-            decoration: BoxDecoration(
-                color: Colors.pinkAccent,
-                //borderRadius: BorderRadius.only(
-                //  topLeft: Radius.circular(21), bottomRight: Radius.circular(25)
-                // ),
-                border: Border.all(
-                    width: 2,
-                    color: Colors.cyan
-                ),
-                boxShadow: [
-                  BoxShadow(
-                    blurRadius: 11,
-                    spreadRadius: 7,
-                  )
-                ],shape: BoxShape.rectangle ),
-          ),
-          Container(
-            height: 100,
-            width: 200,
-            decoration: BoxDecoration(
-                color: Colors.greenAccent,
-                //borderRadius: BorderRadius.only(
-                //  topLeft: Radius.circular(21), bottomRight: Radius.circular(25)
-                // ),
-                border: Border.all(
-                    width: 2,
-                    color: Colors.cyan
-                ),
-                boxShadow: [
-                  BoxShadow(
-                    blurRadius: 11,
-                    spreadRadius: 7,
-                  )
-                ],shape: BoxShape.rectangle ),
-          ), Container(
-            height: 100,
-            width: 200,
-            decoration: BoxDecoration(
-                color: Colors.pinkAccent,
-                //borderRadius: BorderRadius.only(
-                //  topLeft: Radius.circular(21), bottomRight: Radius.circular(25)
-                // ),
-                border: Border.all(
-                    width: 2,
-                    color: Colors.cyan
-                ),
-                boxShadow: [
-                  BoxShadow(
-                    blurRadius: 11,
-                    spreadRadius: 7,
-                  )
-                ],shape: BoxShape.rectangle ),
-          ),
-          Container(
-            height: 100,
-            width: 200,
-            decoration: BoxDecoration(
-                color: Colors.greenAccent,
-                //borderRadius: BorderRadius.only(
-                //  topLeft: Radius.circular(21), bottomRight: Radius.circular(25)
-                // ),
-                border: Border.all(
-                    width: 2,
-                    color: Colors.cyan
-                ),
-                boxShadow: [
-                  BoxShadow(
-                    blurRadius: 11,
-                    spreadRadius: 7,
-                  )
-                ],shape: BoxShape.rectangle ),
-          ), Container(
-            height: 100,
-            width: 200,
-            decoration: BoxDecoration(
-                color: Colors.pinkAccent,
-                //borderRadius: BorderRadius.only(
-                //  topLeft: Radius.circular(21), bottomRight: Radius.circular(25)
-                // ),
-                border: Border.all(
-                    width: 2,
-                    color: Colors.cyan
-                ),
-                boxShadow: [
-                  BoxShadow(
-                    blurRadius: 11,
-                    spreadRadius: 7,
-                  )
-                ],shape: BoxShape.rectangle ),
-          ),
-          Container(
-            height: 100,
-            width: 200,
-            decoration: BoxDecoration(
-                color: Colors.greenAccent,
-                //borderRadius: BorderRadius.only(
-                //  topLeft: Radius.circular(21), bottomRight: Radius.circular(25)
-                // ),
-                border: Border.all(
-                    width: 2,
-                    color: Colors.cyan
-                ),
-                boxShadow: [
-                  BoxShadow(
-                    blurRadius: 11,
-                    spreadRadius: 7,
-                  )
-                ],shape: BoxShape.rectangle ),
-          ),
-        ],
-      ),
-    ),
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(
+          "Hello",
+          style: TextStyle(fontWeight: FontWeight.bold, color: Colors.white),
+        ),
+        backgroundColor: Colors.brown,
+      ),body: ListView.builder(itemBuilder: (context,index){
+        return Text('One',style: TextStyle(fontSize: 25),);
+    }
+    ,itemCount: 5,)
 
-
-        );
+    );
   }
 }

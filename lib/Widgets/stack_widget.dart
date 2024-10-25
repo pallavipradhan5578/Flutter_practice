ee15b843a92c4d5fae7dc66f8077b56d15704bc9
pallavipradhan5578 <pallavipradhan5578@gmail.com>
2024-10-25 16:55:42 +0530

    alert widet.dart

--- lib/Widgets/stack_widget.dart
+++ lib/Widgets/stack_widget.dart
@@ -1,4 +1,6 @@
-import 'package:flutter/material.dart';class Stack_widgwt_page extends StatefulWidget {
+import 'package:flutter/material.dart';
+
+class Stack_widgwt_page extends StatefulWidget {
   const Stack_widgwt_page({super.key});
 
   @override
@@ -9,51 +11,38 @@ class _Stack_widgwt_pageState extends State<Stack_widgwt_page> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(backgroundColor: Colors.brown,),body: Stack(
-        children: [Padding(
-          padding: const EdgeInsets.all(20.0),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              Container(color: Colors.greenAccent,
-
-
-                  height: MediaQuery.of(context).size.width / 2,
-
-
-                ),
-
-
-              SizedBox(height: 20,),
-              Text('',style: TextStyle(fontSize: 25,fontWeight: FontWeight.w300),),
-
-
-              Text('',style: TextStyle(fontSize: 25,fontWeight: FontWeight.w700)),
-              Text('',style: TextStyle(fontSize: 25,fontWeight: FontWeight.w300)),
-            ],
-          ),
-        ),
-          Positioned(
-            right: 40,
-            top: 30,
-            child: Container(
-              height: 25,
-              width: 80,
-              child: Center(
-                child: Text(
-                  "Status",
-                  style: TextStyle(
-                      color: Colors.white),
-                ),
-              ),
-              decoration: BoxDecoration(
-                  color: Colors.green,
-                  borderRadius:
-                  BorderRadius.circular(
-                      12)),
-            ),
-          ),]
-    ),
+      appBar: AppBar(
+        backgroundColor: Colors.brown,
+      ),
+      body: Center(
+        child: ElevatedButton(
+            style: ElevatedButton.styleFrom(backgroundColor: Colors.green,minimumSize: Size(250, 50)),
+            onPressed: () {
+              showDialog(
+                  context: context,
+                  builder: (context) {
+                    return Container(
+                      child: AlertDialog(
+                        title: Text('Do you want to exit ?'),
+                        actions: [
+                          TextButton(
+                            onPressed: () {
+                              Navigator.pop(context);
+                            },
+                            child: Text("Yes"),
+                          ),
+                          TextButton(
+                              onPressed: () {
+                                Navigator.pop(context);
+                              },
+                              child: Text("No"))
+                        ],
+                      ),
+                    );
+                  });
+            },
+            child: Text("Login")),
+      ),
     );
   }
 }
